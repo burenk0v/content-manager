0a3ea27a7eacb23dcf0fd1439fbccac77c2394d7
Oleg Burenkov <burenkov.oleg@gmail.com>
2026-09-16 00:15:03 +0300

    feat: add publisher adapter package

---
+++ telegram/src/providers/__init__.py
@@ -0,0 +1,4 @@
+from .base import PublicationContext, PublicationResult, Publisher
+from .telegram import TelegramPublisher
+
+__all__ = ["PublicationContext", "PublicationResult", "Publisher", "TelegramPublisher"]
