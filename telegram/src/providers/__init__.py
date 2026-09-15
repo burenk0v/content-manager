8b97fb08cd1c03dd7703e9588c15f5792caa8e54
Oleg Burenkov <burenkov.oleg@gmail.com>
2026-09-16 00:23:21 +0300

    refactor: expose publication provider registry

--- telegram/src/providers/__init__.py
+++ telegram/src/providers/__init__.py
@@ -1,4 +1,13 @@
 from .base import PublicationContext, PublicationResult, Publisher
+from .registry import ProviderRegistry, UnsupportedPublisherError, registry
 from .telegram import TelegramPublisher
 
-__all__ = ["PublicationContext", "PublicationResult", "Publisher", "TelegramPublisher"]
+__all__ = [
+    "PublicationContext",
+    "PublicationResult",
+    "Publisher",
+    "ProviderRegistry",
+    "UnsupportedPublisherError",
+    "TelegramPublisher",
+    "registry",
+]
